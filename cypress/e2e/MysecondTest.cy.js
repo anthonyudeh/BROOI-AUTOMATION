d628a36d7ae930f3389cfd59029ccd1c098374b8
Anthony Udeh Chinaza <anthonyudehchinaza@gmail.com>
2023-03-22 08:42:06 -0700

    testing amazon input

---
+++ cypress/e2e/MysecondTest.cy.js
@@ -0,0 +1,24 @@
+
+
+describe("css selectors", ()=>{
+    it("first test using css selectors", ()=>{
+        //visit the applicaion -- first step
+
+        cy.visit("https://www.amazon.com/ref=nav_logo")
+
+        //access the search element and pass some element inside 
+        
+
+        //step 1---- get the id/class of input 
+        //step 2---- .type("to search on the input ")
+        cy.get("#twotabsearchtextbox").type("books on sell")
+
+
+    }); 
+
+    it("second test", ()=>{
+
+         //step 3--- grab the search icon class and use the click method
+         cy.get("#nav-search-submit-button").click()
+    })
+})
