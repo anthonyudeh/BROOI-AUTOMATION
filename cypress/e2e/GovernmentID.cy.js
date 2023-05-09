465cba66b43e80c833d1f1a85cfabee4d54aed1b
Anthony Udeh Chinaza <anthonyudehchinaza@gmail.com>
2023-05-09 16:15:03 -0700

    Government-ID

--- cypress/e2e/GovernmentID.cy.js
+++ cypress/e2e/GovernmentID.cy.js
@@ -1,6 +1,6 @@
 
 
-describe("css selectors", () => {
+describe("Validating Government Identification", () => {
   it('Verify Government ID --Postive test', () => {
 
     //user login
@@ -46,149 +46,28 @@ describe("css selectors", () => {
 
     //Choose an ID  type verification
     cy.get('div.ebq526p').should('equ', 'Identity verification')
-
     cy.get('div._1vb9n5m').should('equ', 'Choose an ID type to add')
+    cy.get('.location-country-select').select('Nigeria').should('have.value')
+    cy.get("input[name='upload-id']").eq(0).check().should('be.checkerd')
+    cy.get("input[name='upload-id']").eq(1).should('not.be.checked')
+    cy.get("input[name='upload-id']").eq(2).should('not.be.checked')
 
-    cy.get('select.location-country-select').click()
 
-    cy.get('option').eq(3).click()
+    //validate the text
+    cy.get('._pog3hg > span > div').should('containt', 'Your ID will be handled according to our Privacy Policy')
 
-    cy.get('input[type=radio]').equ(1).click()
+    cy.get('button._74ax9g3').click()
 
-    //validating back button
-    // cy.get("._pa35zs").click()
 
-    //Validating continue button 
-    cy.get('button._74ax9g3').clck()
+    //Upload images of your drivers license 
+    cy.url().should('eq','https://staging.brooi.com/account-settings/personal-info/government-id/upload-identity/drivers-license')
+    cy.get('div.ebq526p').should('equ', 'Identity verification')
+    cy.get('._1vb9n5m').should('equ', "Upload images of your driver's licence")
+    cy.get('div._1o5t7rw > span > span').should('equ', "Make sure your photos aren’t blurry and the front of your driver’s license clearly shows your face.")
 
   })
 
 
-  // it('government - ID', () => {
-
-  //     cy.visit("https://staging.brooi.com/account-settings/personal-info")
-
-  //     cy.get("span").should("Sign in").click()
-
-  //     cy.get("button.edit--btn").eq(5).click()
-
-  //     cy.get("button._74ax9g3").click()
-
-  //     cy.get("div._pa35zs").eq(0).click()
-
-  //     cy.get("div._pa35zs").eq(1).click()
-
-  //     cy.get("div.ranq7o").eq(0).click()
-
-  //     cy.get("button._1qosfpsp").click()
 
 
-
-  // assertions ---> are divided into two implicit and explicit in cypress 
-  // implicit --> and, should 
-  // explicit --> assert, expect 
-
-  /*
-    describe("Assertions", ()=>{
-        it("Implicit assertion",()=>{
-
-             cy.visit("staging.brooi.com")
-
-             //should for validation  of url
-             cy.url().should('include','brooi.com') //we are saying it should include a url 
-             cy.url().should('eq', 'staging.brooi.com') //shoud equal the url 
-             cy.url().should('contain', 'staging')
-
-
-             cy.url().should('include', 'staging.com' )
-             .should('eq', 'staging.brooi.com') 
-             .should('contain', 'staging')
-
-
-             //and 
-               cy.url().should('include', 'staging.com' )
-             .and('eq', 'staging.brooi.com') 
-             .and('contain', 'staging')
-
-
-             //positive assertions for img 
-             cy.get('.brooi-svg-logo > img').should(be.visible).and('exist')
-             cy.get('.brooi-svg-logo > img').should('exist')
-
-             //verify the value of an input --> let's say we want to validate the information the user has entered
-             cy.get(".user-firstname-text").type("Hello")
-             cy.get(".user-firstname-text").should('have.value','hello') //this is a value check 
-
-              
-             
-             //-----Negative assertions
-             cy.url().should('not.contain', ' Hello world')
-
-
-            //title of the web page 
-            cy.title().should('eq','Brooi')
-
-            Explicit assertions ---> 
-            expect and assert 
-
-            ------------
-            expect --BDD
-            ------------
-
-            it('explict assertion BDD', ()=>{
-
-
-              //Behavior-driven development (BDD) is an Agile software development methodology in which an application is documented and designed around the behavior a user
-
-
-              cy.visit("hebyr://www.wfbffb.com")
-              cy.get('input[placeholder="username"]').type("hello")
-               cy.get('input[placeholder="username"]').type("hello")
-               cy.get("click-button").click()
-
-               //javascript fnc 
-               let expectedName = "xyz"
-
-               cy.get(".this-dhryl").then((x)=>{
-
-                let actualName = x.text()
-                expect(actualName).to.equal(expectedName)
-                
-                  
-               })
-            })
-
-
-
-            it('explicit asssertion TDD style', ()=>{
-
-
-              //Test-driven development (TDD) is a software development process relying on software requirements being converted to test cases before software is fully ...
-
-                        cy.visit("hebyr://www.wfbffb.com")
-              cy.get('input[placeholder="username"]').type("hello")
-               cy.get('input[placeholder="username"]').type("hello")
-               cy.get("click-button").click()
-
-               //javascript fnc 
-               let expectedName = "xyz"
-
-
-              cy.get(".this-is").then((x)=>{
-
-                let actualname = x.text()
-
-                //TDD Style
-                assert.equal(actualname,expectedname)
-                assert.not.equal(actualname, expectedname)
-
-              })
-
-            })
-            assert -TDD 
-        })
-         
-     }) 
-  */
-  // })
 })
